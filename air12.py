896816f535c727424720890f1b1d6e49738f29af
samuel-risch <rischebourg.samuel@gmail.com>
2023-12-13 20:24:45 +0100

    Le roi des tris

---
+++ air12.py
@@ -0,0 +1,36 @@
+# Module et Fonctions
+
+import sys
+    
+def partition(array, low, high):
+    pivot = array[high]
+    i = low - 1
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i + 1
+            (array[i], array[j]) = (array[j], array[i])
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+    return i + 1
+ 
+ 
+def quicksort(array, low, high):
+    if low < high:
+        pi = partition(array, low, high)
+        quicksort(array, low, pi - 1)
+        quicksort(array, pi + 1, high)
+ 
+# Parsing
+
+array = sys.argv[1:] 
+
+# Resolution et Affichage
+
+N = len(array) 
+quicksort(array, 0, N - 1)
+for x in array:
+    print(x, end=" ")   
+print()
+
+
+
+  
