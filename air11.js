6046211d08bdd261d03c62035cb4466739c5b508
Rischh <rischebourg.samuel@gmail.com>
2025-03-24 16:06:01 +0100

    feat: air11 in js

---
+++ air11.js
@@ -0,0 +1,47 @@
+const getPyramid = (symbol, numberFloor) => {
+  let floor = symbol
+
+  for (let i = 0; i < numberFloor; i++) {
+    let symbolNumber = 1
+    for (let j = i + 1; j < numberFloor; j++) {
+      symbolNumber += 2
+    }
+    let spaceNumber = (symbolNumber - 1) / 2
+    let space = ""
+    for (let k = 0; k < spaceNumber; k++) {
+      space += " "
+    }
+    console.log(space + floor + space)
+    floor += symbol + symbol
+  }
+}
+
+const validateArgsCount = (args) => {
+  if (args.length !== 2)
+    return console.error(
+      "Le programme a besoin de 2 arguments pour fonctionner."
+    )
+  return args
+}
+
+const validateNumericArg = (arg) => {
+  if (isNaN(arg))
+    return console.error(
+      "Le programme a besoin d'un nombre comme deuxieme argument pour definir les etages."
+    )
+  return +arg
+}
+
+const getArgs = () => process.argv.slice(2)
+
+const displayPyramid = () => {
+  const args = validateArgsCount(getArgs())
+  if (!args) return
+
+  const numberFloor = validateNumericArg(args[1])
+  if (!numberFloor) return
+
+  return getPyramid(args[0], numberFloor)
+}
+
+displayPyramid()
