d476f8a26f99b7652db3519999c006cf83df97bf
Rischh <rischebourg.samuel@gmail.com>
2025-03-28 15:11:04 +0100

    feat: air10 in js

---
+++ air10.js
@@ -0,0 +1,40 @@
+const fs = require("fs")
+
+const getContentFile = (fileName) => {
+  const data = fs.readFileSync(fileName, "utf8", (err) => {
+    if (err) return console.error(err)
+  })
+
+  return data
+}
+
+const isValidArgsLength = (args) => {
+  if (args.length !== 1)
+    return console.error(
+      "Le programme a besoin d'un argument pour fonctionner."
+    )
+  return args
+}
+
+const isValidString = (string) => {
+  if (!isNaN(string))
+    return console.error(
+      "Le programme a besoin d'une chaine de caracteres comme argument pour fonctionner."
+    )
+  return string
+}
+
+const getArgs = () => process.argv.slice(2)
+
+const resolveContentFile = () => {
+  const args = isValidArgsLength(getArgs())
+  if (!args) return
+
+  const string = isValidString(args[0])
+  if (!string) return
+
+  const fileName = string
+  return getContentFile(fileName)
+}
+
+console.log(resolveContentFile())
