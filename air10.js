fe303665303bc8262f8d2762b2117e7c527acd3b
Rischh <rischebourg.samuel@gmail.com>
2025-05-09 11:10:02 +0200

    refactor(air10): delete useless EH functions

--- air10.js
+++ air10.js
@@ -1,7 +1,7 @@
-const fs = require("node:fs")
-
 const getContentFile = (fileName) => {
-  const content = fs.readFileSync(fileName, "utf8")
+  const { readFileSync } = require("node:fs")
+
+  const content = readFileSync(fileName, "utf8")
   return content
 }
 
@@ -11,38 +11,12 @@ const isValidLength = (validLength) => {
   return true
 }
 
-const isValidString = (string) => {
-  if (!isNaN(string))
-    return console.error(
-      "Le programme a besoin d'une chaine de caracteres comme argument pour fonctionner."
-    )
-  return string
-}
-
 const isValidFileExtension = (fileName, validFileExtension) => {
   const fileExtension = fileName.split(".")[1]
 
   if (fileExtension !== validFileExtension)
     return console.error("L'extension du fichier n'est pas valide.")
   return fileName
-}
-
-const isExistingFile = (filePath) => {
-  const isExisting = fs.existsSync(filePath)
-
-  if (!isExisting)
-    return console.error("Le fichier n'existe pas dans le dossier courrant.")
-  return true
-}
-
-const isReadableFile = (filePath) => {
-  try {
-    fs.accessSync(filePath, fs.constants.R_OK)
-    return true
-  } catch (error) {
-    console.error("Le fichier specifie n'est pas lisible.")
-    return false
-  }
 }
 
 const getArgs = () => {
@@ -56,14 +30,8 @@ const resolveContentFile = () => {
 
   if (!isValidLength(validLength)) return
 
-  const string = isValidString(args[0])
-  if (!string) return
-
   const fileName = isValidFileExtension(string, "txt")
   if (!fileName) return
-
-  if (!isExistingFile(fileName)) return
-  if (!isReadableFile(fileName)) return
 
   return getContentFile(fileName)
 }
